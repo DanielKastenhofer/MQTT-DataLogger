282b0654122f796f5b12b14363eee6b90223e776
Daniel <daniel.kastenhofer1@gmail.com>
2021-06-09 07:24:55 +0200

    Added Data again

---
+++ MQTT_DataLogger/Data/Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTT_DataLogger.Data
+{
+    class Device
+    {
+        public int ID { get; set; }
+
+        public Temperature Temp { get; set; }
+
+        public Usage Usage { get; set; }
+    }
+}
---
+++ MQTT_DataLogger/Data/Usage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTT_DataLogger.Data
+{
+    class Usage
+    {
+        public int ID { get; set; }
+
+        public double Value { get; set; }
+
+        public Device Device { get; set; }
+
+    }
+}
